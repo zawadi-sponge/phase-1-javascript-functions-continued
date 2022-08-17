84e522a33fed2536996ef534f7a7637fb677cfa6
Zawadi <zawadi.opoti@student.moringaschool.com>
2022-08-17 10:14:11 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(Axel){
+ if(Axel === 'bathe my dog'){
+    return 'This Saturday, I want to bathe my dog!'
+ }
+ return 'This Saturday, I want to roller-skate!'
+}
+
+function mondayWork(Axel){
+if(Axel === 'work from home'){
+    return 'This Monday, I will work from home.'
+}
+return "This Monday, I will go to the office."
+}
+
+function wrapAdjective(worker){
+    return function (Axel = 'fired'){
+return `You are ${worker}${Axel}${worker}!`
+    }
+}
